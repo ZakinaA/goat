4307b10f62ac274f120f21fa6478dad23509afee
unknown <55048956+JLEGit@users.noreply.github.com>
2022-11-14 15:07:40 +0100

    init

---
+++ src/Entity/Cours.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CoursRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CoursRepository::class)]
+class Cours
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $libelle = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $agemini = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $agemaxi = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $nbplaces = null;
+
+    #[ORM\ManyToMany(targetEntity: Eleve::class, mappedBy: 'inscription')]
+    private Collection $eleves;
+
+    public function __construct()
+    {
+        $this->eleves = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getAgemini(): ?int
+    {
+        return $this->agemini;
+    }
+
+    public function setAgemini(?int $agemini): self
+    {
+        $this->agemini = $agemini;
+
+        return $this;
+    }
+
+    public function getAgemaxi(): ?int
+    {
+        return $this->agemaxi;
+    }
+
+    public function setAgemaxi(?int $agemaxi): self
+    {
+        $this->agemaxi = $agemaxi;
+
+        return $this;
+    }
+
+    public function getNbplaces(): ?int
+    {
+        return $this->nbplaces;
+    }
+
+    public function setNbplaces(?int $nbplaces): self
+    {
+        $this->nbplaces = $nbplaces;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Eleve>
+     */
+    public function getEleves(): Collection
+    {
+        return $this->eleves;
+    }
+
+    public function addElefe(Eleve $elefe): self
+    {
+        if (!$this->eleves->contains($elefe)) {
+            $this->eleves->add($elefe);
+            $elefe->addInscription($this);
+        }
+
+        return $this;
+    }
+
+    public function removeElefe(Eleve $elefe): self
+    {
+        if ($this->eleves->removeElement($elefe)) {
+            $elefe->removeInscription($this);
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Responsable.php
@@ -0,0 +1,210 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ResponsableRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ResponsableRepository::class)]
+class Responsable
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $adresse = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $ville = null;
+
+    #[ORM\Column(length: 5, nullable: true)]
+    private ?string $code_postal = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 10, nullable: true)]
+    private ?string $tel1 = null;
+
+    #[ORM\Column(length: 10, nullable: true)]
+    private ?string $tel2 = null;
+
+    #[ORM\Column(length: 10, nullable: true)]
+    private ?string $tel3 = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $quotient_familial = null;
+
+    #[ORM\OneToMany(mappedBy: 'responsable', targetEntity: Eleve::class)]
+    private Collection $eleves;
+
+    public function __construct()
+    {
+        $this->eleves = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(?string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(?string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(?string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getCodePostal(): ?string
+    {
+        return $this->code_postal;
+    }
+
+    public function setCodePostal(?string $code_postal): self
+    {
+        $this->code_postal = $code_postal;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getTel1(): ?string
+    {
+        return $this->tel1;
+    }
+
+    public function setTel1(?string $tel1): self
+    {
+        $this->tel1 = $tel1;
+
+        return $this;
+    }
+
+    public function getTel2(): ?string
+    {
+        return $this->tel2;
+    }
+
+    public function setTel2(?string $tel2): self
+    {
+        $this->tel2 = $tel2;
+
+        return $this;
+    }
+
+    public function getTel3(): ?string
+    {
+        return $this->tel3;
+    }
+
+    public function setTel3(?string $tel3): self
+    {
+        $this->tel3 = $tel3;
+
+        return $this;
+    }
+
+    public function getQuotientFamilial(): ?int
+    {
+        return $this->quotient_familial;
+    }
+
+    public function setQuotientFamilial(?int $quotient_familial): self
+    {
+        $this->quotient_familial = $quotient_familial;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Eleve>
+     */
+    public function getEleves(): Collection
+    {
+        return $this->eleves;
+    }
+
+    public function addElefe(Eleve $elefe): self
+    {
+        if (!$this->eleves->contains($elefe)) {
+            $this->eleves->add($elefe);
+            $elefe->setResponsable($this);
+        }
+
+        return $this;
+    }
+
+    public function removeElefe(Eleve $elefe): self
+    {
+        if ($this->eleves->removeElement($elefe)) {
+            // set the owning side to null (unless already changed)
+            if ($elefe->getResponsable() === $this) {
+                $elefe->setResponsable(null);
+            }
+        }
+
+        return $this;
+    }
+}
